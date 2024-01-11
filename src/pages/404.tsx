70e7ec4d9770b465f4ec3eaca9879b2a738c6a34
jspvg <josipovic1501@gmail.com>
2024-01-11 18:05:46 +0100

    added simple landing and 404 pages, improved url handling

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import '../styles/components/layout.scss';
+
+const PageNotFound = () => {
+  return (
+    <div className="message">
+      <p>The page you are looking for does not exist</p>
+    </div>
+  );
+};
+
+export default PageNotFound;
